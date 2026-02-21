529f622fa51465fdd92879ddda69a6e62b07b978
Péter Jeszenszky <jeszenszky.peter@inf.unideb.hu>
2026-02-21 14:42:55 +0100

    fix: implement toString() in Nationality with ReflectionToStringBuilder

--- src/main/java/nationalize/Nationality.java
+++ src/main/java/nationalize/Nationality.java
@@ -1,6 +1,8 @@
 package nationalize;
 
 import com.google.gson.annotations.SerializedName;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 import java.util.List;
 
@@ -42,6 +44,11 @@ public class Nationality {
         this.countries = countries;
     }
 
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.toString(this, ToStringStyle.NO_CLASS_NAME_STYLE);
+    }
+
     public static class Country {
 
         private String countryId;
@@ -66,6 +73,11 @@ public class Nationality {
 
         public void setProbability(float probability) {
             this.probability = probability;
+        }
+
+        @Override
+        public String toString() {
+            return ReflectionToStringBuilder.toString(this, ToStringStyle.NO_CLASS_NAME_STYLE);
         }
 
     }
